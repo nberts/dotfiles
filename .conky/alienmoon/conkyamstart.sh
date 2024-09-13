1a4c8185c479f01f3cebc6648137d7aed4827d9d
Nina Bertrand <nina@ninabsavage.ca>
2024-09-13 11:10:06 -0400

    initial commit

---
+++ .conky/alienmoon/conkyamstart.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+sleep 1
+conky -q -c /home/nina/.conky/alienmoon/.conkyamrc & 
+exit
---
+++ .conky/conky1/conkystart.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+sleep 1
+conky -q -c /home/nina/.conky/conky1/.conkyrc1 & 
+conky -q -c /home/nina/.conky/conky1/.conkyrc2 & 
+exit
